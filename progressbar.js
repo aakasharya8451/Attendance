c32e563d0f40a7e2c96df4930a20232ebeaf13b7
Aakash Arya <109866429+aakasharya8451@users.noreply.github.com>
2023-12-17 17:19:26 +0530

    Circular Progress bar added

---
+++ progressbar.js
@@ -0,0 +1,75 @@
+class AttendanceProgressBarCard {
+    constructor(title, percent) {
+        this.title = title;
+        this.percent = percent;
+        this.determineColor();
+    }
+
+    determineColor() {
+        if (this.percent > 85) {
+            this.strokeColor = "#2ecc71"; // Green
+        } else if (this.percent >= 75 && this.percent <= 85) {
+            this.strokeColor = "#f39c12"; // Orange
+        } else {
+            this.strokeColor = "#e74c3c"; // Red
+        }
+    }
+
+    createCard() {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const percentDiv = document.createElement("div");
+        percentDiv.className = "percent";
+
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const circle1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        const circle2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+        circle1.setAttribute("cx", "105");
+        circle1.setAttribute("cy", "105");
+        circle1.setAttribute("r", "100");
+
+        circle2.setAttribute("cx", "105");
+        circle2.setAttribute("cy", "105");
+        circle2.setAttribute("r", "100");
+        circle2.style.setProperty("--percent", this.percent);
+
+        svg.appendChild(circle1);
+        svg.appendChild(circle2);
+
+        const numberDiv = document.createElement("div");
+        numberDiv.className = "number";
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = `${this.percent}<span>%</span>`;
+
+        numberDiv.appendChild(h3);
+
+        percentDiv.appendChild(svg);
+        percentDiv.appendChild(numberDiv);
+
+        const titleDiv = document.createElement("div");
+        titleDiv.className = "title";
+
+        const h2 = document.createElement("h2");
+        h2.innerHTML = this.title;
+
+        titleDiv.appendChild(h2);
+
+        card.appendChild(percentDiv);
+        card.appendChild(titleDiv);
+
+          // Set stroke color
+        if (this.strokeColor) {
+            circle2.style.setProperty("--stroke-color", this.strokeColor);
+        }
+
+        var test = document.getElementById("CA355");
+        console.log(test)
+        test.appendChild(card);
+        }
+      }
+
+const CA355Card = new AttendanceProgressBarCard("CA355", 30);
+CA355Card.createCard();
